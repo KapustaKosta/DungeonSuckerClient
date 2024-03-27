3b88b686753af33b2da49a5589790e86ed2e007c
IlyaKaput <ilya9999_ilya@mail.ru>
2024-03-27 16:47:24 +0300

    Added SwordGenerator; Just as ClubGenerator it will work when we discuss rarity system and element system (element system is optional)

--- core/src/com/mipt/tp/dungeon_sucker/gameplay/generators/WeaponGenerator.java
+++ core/src/com/mipt/tp/dungeon_sucker/gameplay/generators/WeaponGenerator.java
@@ -3,12 +3,14 @@ package com.mipt.tp.dungeon_sucker.gameplay.generators;
 import com.mipt.tp.dungeon_sucker.gameplay.generators.Sets.RaritySet;
 import com.mipt.tp.dungeon_sucker.gameplay.generators.weaponGenerators.ClubGenerator;
 import com.mipt.tp.dungeon_sucker.gameplay.generators.Sets.ElementSet;
+import com.mipt.tp.dungeon_sucker.gameplay.generators.weaponGenerators.SwordGenerator;
 import com.mipt.tp.dungeon_sucker.gameplay.items.Weapon;
+import com.mipt.tp.dungeon_sucker.gameplay.items.Weapons.WeaponsForPlayer.Sword;
 
 import java.util.Random;
 
 public class WeaponGenerator {
-  final int AmountOfWeaponTypesInGame = 1;
+  final int AmountOfWeaponTypesInGame = 2;
   ElementSet elementSet = new ElementSet();
   RaritySet raritySet = new RaritySet();
   public Weapon generateWeapon(int level){
@@ -18,6 +20,7 @@ public class WeaponGenerator {
     level = level + new Random().nextInt(3) - 1;
     switch(WhatToCreate){
       case 0: return new ClubGenerator().genetateClub(rarity, element, level);
+      case 1: return new SwordGenerator().generateSword(rarity, element, level);
     }
     throw new IndexOutOfBoundsException();
   }
---
+++ core/src/com/mipt/tp/dungeon_sucker/gameplay/generators/weaponGenerators/SwordGenerator.java
@@ -0,0 +1,12 @@
+package com.mipt.tp.dungeon_sucker.gameplay.generators.weaponGenerators;
+
+import com.mipt.tp.dungeon_sucker.gameplay.items.Weapon;
+import com.mipt.tp.dungeon_sucker.gameplay.items.Weapons.WeaponsForPlayer.Sword;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SwordGenerator {
+
+  public Weapon generateSword(String rarity, String element, int level) {
+    throw new NotImplementedException();
+  }
+}
