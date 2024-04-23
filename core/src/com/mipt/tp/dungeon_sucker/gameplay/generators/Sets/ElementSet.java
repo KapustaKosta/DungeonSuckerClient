c3f40e13e2e0e5191b1ac857c1631b41b0f5975f
IlyaKaput <ilya9999_ilya@mail.ru>
2024-04-23 20:58:21 +0300

    Renamed Death -> Dark Added new resistances Added vulnerabilities

--- core/src/com/mipt/tp/dungeon_sucker/gameplay/generators/Sets/ElementSet.java
+++ core/src/com/mipt/tp/dungeon_sucker/gameplay/generators/Sets/ElementSet.java
@@ -1,7 +1,5 @@
 package com.mipt.tp.dungeon_sucker.gameplay.generators.Sets;
 
-import java.awt.peer.LightweightPeer;
-
 public enum ElementSet {
-  Fire, Frost, Poison, Death, Light, None
+  Fire, Frost, Poison, Dark, Light, None
 }
--- core/src/com/mipt/tp/dungeon_sucker/gameplay/items/Artifacts/UndoneArtifacts/DarkResistance.java
+++ core/src/com/mipt/tp/dungeon_sucker/gameplay/items/Artifacts/UndoneArtifacts/DarkResistance.java
@@ -4,18 +4,18 @@ import com.mipt.tp.dungeon_sucker.gameplay.Damage;
 import com.mipt.tp.dungeon_sucker.gameplay.generators.Sets.ElementSet;
 import com.mipt.tp.dungeon_sucker.gameplay.items.Artifact;
 
-public class DeathResistance extends Artifact {
-  public DeathResistance() {
+public class DarkResistance extends Artifact {
+  public DarkResistance() {
     super();
     this.triggerableByBeingDamaged = true;
     this.weight = 0;
     this.effectiveness = 3;
-    this.description = "Death damage is twice less effective";
-    this.name = "Death damage Resistance";
+    this.description = "Dark damage is twice less effective";
+    this.name = "Dark Resistance";
   }
 
   public void triggerByBeingDamaged(Damage damage) {
-    if (damage.element == ElementSet.Death) {
+    if (damage.element == ElementSet.Dark) {
       damage.elementDamage /= 2;
       damage.totalDamage = damage.defaultDamage + damage.elementDamage;
     }
